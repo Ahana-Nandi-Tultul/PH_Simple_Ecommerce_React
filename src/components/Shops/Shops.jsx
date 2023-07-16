1199b9af6062d7728c0180a69ba1b1250b7f71dc
Ahana-Nandi-Tultul <83442291+Ahana-Nandi-Tultul@users.noreply.github.com>
2023-07-16 12:56:27 +0600

    item add to cart + favicon

--- src/components/Shops/Shops.jsx
+++ src/components/Shops/Shops.jsx
@@ -1,27 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import './Shops.css';
 import Product from '../Product/Product';
+
  
 
 const Shops = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
     useEffect(() => {
         fetch('products.json')
         .then(res => res.json())
         .then(data => setProducts(data))
     }, []);
-    const addToCart = (id) => {
-        console.log('clicked id', id);
+    const handleAddToCart = (product) => {
+        const newCart = [...cart, product];
+        setCart(newCart);
     }
+
     return (
         <div className='shops-container'>
             <div className='products-container'>
                 {
-                    products.map(product => <Product product ={product} key={product.id} addToCart={addToCart}></Product>)
+                    products.map(product => <Product 
+                        product ={product} 
+                        key={product.id} 
+                        handleAddToCart={handleAddToCart}></Product>)
                 }
             </div>
             <div>
                 <h1>Order Summary</h1>
+                <p>Selected Items: {cart.length}</p>
             </div>
         </div>
     );
